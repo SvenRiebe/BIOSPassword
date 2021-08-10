0a1bbab5d3d8335b4e8e0ad81302346889ab0a1b
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-10 15:29:31 +0200

    Create ws1_sensor_dell_bios_profile_status_v1_0_0.ps1

---
+++ ws1_sensor_dell_bios_profile_status_v1_0_0.ps1
@@ -0,0 +1,17 @@
+# Description
+# Return Type: String
+# Execution Context: System
+##################################################################
+#
+# Name: Sensor Dell status AdminPW by BIOS Profile
+#
+# Author: Sven Riebe
+#
+# Status: test
+#
+# Version 1.0.0
+#
+# Date: 08-06-2021
+
+$BiosProfilStatus = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Dell\BIOS\' -Name Status | select -ExpandProperty Status
+Write-Output $BiosProfilStatus
