a134d1a02826e5307e12118c8171af36c75e2ab9
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-10 14:47:28 +0200

    Create ws1_sensor_dell_bios_profile_date_v1_0_0.ps1

---
+++ ws1_sensor_dell_bios_profile_date_v1_0_0.ps1
@@ -0,0 +1,17 @@
+# Description
+# Return Type: String
+# Execution Context: System
+##################################################################
+#
+# Name: Sensor Dell status change date AdminPW by BIOS Profile
+#
+# Author: Sven Riebe
+#
+# Status: test
+#
+# Version 1.0.0
+#
+# Date: 08-06-2021
+
+$BiosProfilDate = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Dell\BIOS\' -Name Date | select -ExpandProperty Date
+Write-Output $BiosProfilDate
